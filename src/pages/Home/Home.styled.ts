17d91726b82fda953f1077f127ab38345b40d143
Oktay Shakirov <oktayshakirov@gmail.com>
2023-07-08 23:15:21 +0200

    update
    - testing new logo
    - added floating effect to the logo
    - added 3 blog posts cards to home page
    - responsive design modifications

--- src/pages/Home/Home.styled.ts
+++ src/pages/Home/Home.styled.ts
@@ -2,15 +2,16 @@ import { css } from '@emotion/react';
 import { styled } from '@mui/material/styles';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
 
 export const StyledContainer = styled(Container)`
   ${({ theme }) => css`
-    height: ${theme.spacing(40)};
+    padding-top: ${theme.spacing(2)};
+    padding-bottom: ${theme.spacing(10)};
 
     ${theme.breakpoints.up('md')} {
-      display: flex;
-      align-items: center;
-      height: ${theme.spacing(70)};
+      padding-top: ${theme.spacing(5)};
+      padding-bottom: ${theme.spacing(10)};
     }
   `}
 `;
@@ -19,30 +20,56 @@ export const StyledHeadingContainer = styled(Grid)`
   ${({ theme }) => css`
     display: flex;
     align-items: center;
-    margin-top: ${theme.spacing(10)};
+    margin-bottom: ${theme.spacing(4)};
 
     h2 {
       color: ${theme.palette.text.secondary};
     }
 
     ${theme.breakpoints.up('md')} {
-      margin-top: 0;
+      margin-bottom: ${theme.spacing(0)};
     }
   `}
 `;
 
 export const StyledLogoContainer = styled(Grid)`
   ${({ theme }) => css`
-    position: absolute;
-    top: ${theme.spacing(6)};
-    right: ${theme.spacing(2)};
-    opacity: 0.015;
+    display: flex;
+    justify-content: center;
+    margin-top: ${theme.spacing(2)};
+    flex: 1;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: fit-content;
 
     ${theme.breakpoints.up('md')} {
-      display: flex;
-      justify-content: center;
-      position: static;
-      opacity: 1;
+      margin-top: ${theme.spacing(0)};
     }
+
+    animation: floating 3s ease-in-out infinite;
+
+    @keyframes floating {
+      0% {
+        transform: translateY(0);
+      }
+      50% {
+        transform: translateY(-10px);
+      }
+      100% {
+        transform: translateY(0);
+      }
+    }
+  `}
+`;
+
+export const StyledCard = styled(Card)`
+  ${({ theme }) => css`
+    max-width: 345px;
+    background-color: rgba(0, 0, 0, 0.4);
+    box-shadow: 0 5px 8px rgba(0, 0, 0, 0.2);
+    border-radius: 25px 0 25px 0;
+    margin: auto;
+    margin-top: ${theme.spacing(3)};
+    margin-bottom: ${theme.spacing(3)};
   `}
 `;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,10 +4,16 @@ import Container from '@mui/material/Container';
 import Logo from '@components/Logo';
 import Layout from '@components/Layout';
 import HomeSEO from './Home.SEO';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import ArticleIcon from '@mui/icons-material/Article';
+import NextLink from 'next/link';
 import {
   StyledContainer,
   StyledHeadingContainer,
   StyledLogoContainer,
+  StyledCard,
 } from './Home.styled';
 
 const Home = () => {
@@ -22,14 +28,18 @@ const Home = () => {
                 <Typography component="h1" variant="h3" mb={{ xs: 2, md: 2 }}>
                   Beyond the Buzz
                 </Typography>
-                <Typography component="h2" variant="h6">
-                  Living with tinnitus can be challenging, but you're not alone.
-                  Discover valuable tips and resources to manage and cope with
-                  tinnitus. Explore treatment options, lifestyle adjustments and
-                  support. Empower yourself with comprehensive resources.
-                  Everyone's experience with tinnitus is unique. Find strategies
-                  that suit your needs.
-                </Typography>
+                <Grid item xs={12} md={12}>
+                  {' '}
+                  {/* Wrap the text content in a Grid item */}
+                  <Typography component="h2" variant="h6">
+                    Living with tinnitus can be challenging, but you're not
+                    alone. Discover valuable tips and resources to manage and
+                    cope with tinnitus. Explore treatment options, lifestyle
+                    adjustments and support. Empower yourself with comprehensive
+                    resources. Everyone's experience with tinnitus is unique.
+                    Find strategies that suit your needs.
+                  </Typography>
+                </Grid>
               </div>
             </StyledHeadingContainer>
             <StyledLogoContainer item md={6}>
@@ -38,20 +48,116 @@ const Home = () => {
           </Grid>
         </StyledContainer>
         <Container>
-          <Typography component="h2" variant="h6">
-            Explore Our Articles:
-          </Typography>
-          <Typography component="h2" variant="h6">
+          <Typography component="h2" variant="h6" align="center">
             Discover the fascinating world of perceiving sound in the absence of
-            an external source.
+            an external source :
           </Typography>
-          <Typography component="h2" variant="h6">
-            Tips for Managing Tinnitus:
-          </Typography>
-          <Typography component="h2" variant="h6">
-            Find practical tips and strategies to reduce the impact of tinnitus
-            on your daily life.
-          </Typography>
+          <Grid container spacing={2}>
+            {/* StyledCard Card 1 */}
+            <Grid item xs={12} sm={4}>
+              <StyledCard>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="180"
+                    image="/images/yoga-dog.jpg"
+                    alt="Yoga and Dog"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Managing Tinnitus
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Discover effective strategies for managing tinnitus and
+                      improving your quality of life.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <NextLink href="blog/managing-tinnitus" passHref>
+                    <Button
+                      size="small"
+                      variant="outlined"
+                      color="secondary"
+                      startIcon={<ArticleIcon />}
+                    >
+                      Read More
+                    </Button>
+                  </NextLink>
+                </CardActions>
+              </StyledCard>
+            </Grid>
+
+            {/* StyledCard Card 2 */}
+            <Grid item xs={12} sm={4}>
+              <StyledCard>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="180"
+                    image="/images/anxiety-girl.jpg"
+                    alt="Anxious girl"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      What is Tinnitus ?
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Learn about the perception of sound without an external
+                      source.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <NextLink href="blog/what-is-tinnitus" passHref>
+                    <Button
+                      size="small"
+                      variant="outlined"
+                      color="secondary"
+                      startIcon={<ArticleIcon />}
+                    >
+                      Read More
+                    </Button>
+                  </NextLink>
+                </CardActions>
+              </StyledCard>
+            </Grid>
+
+            {/* StyledCard Card 3 */}
+            <Grid item xs={12} sm={4}>
+              <StyledCard>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="180"
+                    image="/images/girl-with-headphones.jpg"
+                    alt="Girl with headphones"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      The Power of White Noise
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Exploring the benefits and applications of sound therapy
+                      for managing tinnitus.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <NextLink href="blog/the-power-of-white-noise" passHref>
+                    <Button
+                      size="small"
+                      variant="outlined"
+                      color="secondary"
+                      startIcon={<ArticleIcon />}
+                    >
+                      Read More
+                    </Button>
+                  </NextLink>
+                </CardActions>
+              </StyledCard>
+            </Grid>
+          </Grid>
         </Container>
       </Layout>
     </>
