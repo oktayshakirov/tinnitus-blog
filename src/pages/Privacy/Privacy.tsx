a3dcca809e84cd57a436c30a34b814fec832d944
oktayshakirov <oktayshakirov@gmail.com>
2023-09-02 00:29:35 +0200

    update
    fixed issues

--- src/pages/Privacy/Privacy.tsx
+++ src/pages/Privacy/Privacy.tsx
@@ -21,7 +21,7 @@ const Privacy = ({ }: Props) => {
             Automatically Collected Data: When you explore our blog, we might automatically gather specific details about your device, such as information related to your web browser, IP address, time zone, and some cookies installed on your device. Moreover, as you navigate through the site, we gather details about the individual web pages you view and your interactions with the site.
           </p>
           <p>
-            Personal Data: While engaging with our blog, we may prompt you to furnish us with certain personally identifiable information that can be employed to get in touch or identify you. This encompasses, but isn't limited to, your email address.
+            Personal Data: While engaging with our blog, we may prompt you to furnish us with certain personally identifiable information that can be employed to get in touch or identify you. This encompasses, but isn&apos;t limited to, your email address.
           </p>
 
           <Headline>2. How We Use Your Information:</Headline>
@@ -37,27 +37,27 @@ const Privacy = ({ }: Props) => {
 
           <Headline>3. Disclosure of Your Information:</Headline>
           <p>
-            We don't sell, trade, or rent your personal identification information to third parties. However, we might share generic aggregated demographic data, not linked to any personal identification details regarding visitors and users, with our business associates, trusted affiliates, and advertisers for the purposes highlighted above.
+            We don&apos;t sell, trade, or rent your personal identification information to third parties. However, we might share generic aggregated demographic data, not linked to any personal identification details regarding visitors and users, with our business associates, trusted affiliates, and advertisers for the purposes highlighted above.
           </p>
 
           <Headline>4. Cookies:</Headline>
           <p>
-            We deploy cookies to refine the user experience. A cookie is a minuscule file positioned on your computer's hard drive. If desired, you can decline browser cookies by tweaking the appropriate setting on your browser.
+            We deploy cookies to refine the user experience. A cookie is a minuscule file positioned on your computer&apos;s hard drive. If desired, you can decline browser cookies by tweaking the appropriate setting on your browser.
           </p>
 
           <Headline>5. Third-party links and content:</Headline>
           <p>
-            Our blog features links to third-party platforms such as Spotify, Apple Music, Amazon, etc., intended for sound therapy and white noise streaming. These sites come with their distinct privacy policies. We don't bear responsibility or liability for their content or actions.
+            Our blog features links to third-party platforms such as Spotify, Apple Music, Amazon, etc., intended for sound therapy and white noise streaming. These sites come with their distinct privacy policies. We don&apos;t bear responsibility or liability for their content or actions.
           </p>
 
           <Headline>6. Google AdSense:</Headline>
           <p>
-            Google AdSense is used to exhibit ads on our blog. Google deploys cookies to show ads based on a user's past visits to our website or other sites. Users can opt-out of personalized advertising by visiting Google's Ads Settings.
+            Google AdSense is used to exhibit ads on our blog. Google deploys cookies to show ads based on a user&apos;s past visits to our website or other sites. Users can opt-out of personalized advertising by visiting Google&apos;s Ads Settings.
           </p>
 
           <Headline>7. Children’s Privacy:</Headline>
           <p>
-            Our blog isn't designed for children under the age of 13. We don't intentionally gather personal information from children below this age.
+            Our blog isn&apos;t designed for children under the age of 13. We don&apos;t intentionally gather personal information from children below this age.
           </p>
 
           <Headline>8. Modifications to Our Privacy Policy:</Headline>
