bbb0c83338e08b43d297bb31da414228d601183e
Oktay Shakirov <oktayshakirov@gmail.com>
2024-07-05 12:32:19 +0200

    updated g analytics

--- src/const/general.ts
+++ src/const/general.ts
@@ -1,6 +1,4 @@
-import * as process from 'process';
-
 export const DOMAIN = 'https://www.tinnitushelp.me/';
 export const DOMAIN_NAME = 'Tinnitus Help';
 export const DEFAULT_OG_IMAGE = `/images/favicon.ico`;
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ANALYTICS_ID;
+export const GA_TRACKING_ID = `G-YTNX9G7QJN`;
