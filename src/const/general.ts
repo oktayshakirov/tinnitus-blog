a5b8de711a439b2c6902e477b1acb96622c0dea7
Oktay Shakirov <oktayshakirov@gmail.com>
2024-07-05 12:56:25 +0200

    Updated G Tag

--- src/const/general.ts
+++ src/const/general.ts
@@ -1,4 +1,4 @@
 export const DOMAIN = 'https://www.tinnitushelp.me/';
 export const DOMAIN_NAME = 'Tinnitus Help';
 export const DEFAULT_OG_IMAGE = `/images/favicon.ico`;
-export const GA_TRACKING_ID = `G-YTNX9G7QJN`;
+export const GA_TRACKING_ID = `G-KHGG3B9VTM`;
