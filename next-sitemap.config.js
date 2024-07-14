e53b857753de6f67cec0160e5d34a9afe2a773b6
Oktay Shakirov <oktayshakirov@gmail.com>
2024-07-14 12:37:45 +0200

    Updated sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,18 @@
 const config = {
   siteUrl: 'https://www.tinnitushelp.me/',
-  generateRobotsTxt: true, 
+  generateRobotsTxt: true,
+  generateIndexSitemap: false, 
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      }
+    ],
+    additionalSitemaps: [
+      'https://www.tinnitushelp.me/sitemap.xml', 
+    ],
+  },
 };
 
 module.exports = config;
