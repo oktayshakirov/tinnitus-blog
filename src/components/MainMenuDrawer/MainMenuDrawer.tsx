2d3b1d55ec99baa16689904a83ffc4c41088bbfd
Oktay Shakirov <oktayshakirov@gmail.com>
2024-07-25 14:09:15 +0200

    Updated mobile menu

--- src/components/MainMenuDrawer/MainMenuDrawer.tsx
+++ src/components/MainMenuDrawer/MainMenuDrawer.tsx
@@ -26,6 +26,8 @@ const MainMenuDrawer = ({ open, toggleMenu, ...rest }: Props) => {
         <Link href="/zen">Sounds</Link>
         <Link href="/about">About</Link>
         <Link href="/faq">FAQ</Link>
+        <Link href="/privacy">Privacy Policy</Link>
+        <Link href="/terms">Terms of Use</Link>
       </StyledNav>
     </Sidenav>
   );
