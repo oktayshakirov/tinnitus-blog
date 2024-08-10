d561fe2c1eabd5b1e00be4ead72759f2d016bba8
Oktay Shakirov <oktayshakirov@gmail.com>
2024-08-10 14:05:40 +0200

    Updated About / Contact

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -3,6 +3,7 @@ import Container from '@mui/material/Container';
 import { Props } from '@pages/tags';
 import Layout from '@components/Layout';
 import Headline from '@ui/pages/shared/Headline';
+import Link from '@mui/material/Link';
 
 const Contact = ({}: Props) => {
   return (
@@ -10,11 +11,48 @@ const Contact = ({}: Props) => {
       <Layout>
         <Container>
           <Headline>Contact TinnitusHelp.me</Headline>
-          <h2>Contact us at info@tinnitushelp.me</h2>
+
+          <h2>We&apos;d Love to Hear from You</h2>
           <p>
-            For inquiries or further information, you can reach out to me
-            directly via email at oktayshakirov@gmail.com. I highly value the
-            feedback and insights of our readers.
+            Whether you have questions, suggestions, or need support, we&apos;re
+            here to help. At TinnitusHelp.me, we are dedicated to providing
+            valuable resources and insights to help manage and understand
+            tinnitus. Your feedback is essential in helping us improve our
+            content and services.
+          </p>
+
+          <h2>Contact Information</h2>
+          <p>
+            For inquiries, advertising, or partnership opportunities, please
+            feel free to reach out to us:
+          </p>
+          <ul>
+            <li>
+              <strong>Email:</strong>{' '}
+              <Link
+                href="mailto:info@tinnitushelp.me"
+                style={{ color: '#FFFF9B', textDecoration: 'none' }}
+              >
+                info@tinnitushelp.me
+              </Link>
+            </li>
+            <li>
+              <strong>Direct Contact:</strong>{' '}
+              <Link
+                href="mailto:oktayshakirov@gmail.com"
+                style={{ color: '#FFFF9B', textDecoration: 'none' }}
+              >
+                oktayshakirov@gmail.com
+              </Link>
+            </li>
+          </ul>
+
+          <h2>Feedback and Suggestions</h2>
+          <p>
+            We highly value the feedback and insights of our readers. If you
+            have any suggestions on how we can improve our content, website, or
+            services, please don&apos;t hesitate to share them with us. Your
+            input is crucial in helping us improve our content and community.
           </p>
         </Container>
       </Layout>
