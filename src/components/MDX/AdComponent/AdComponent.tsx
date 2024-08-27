eb546ad653abc57962ae5c1a06cc1a141553d4cf
Oktay Shakirov <oktayshakirov@gmail.com>
2024-08-27 13:36:36 +0200

    Updated ads styling

--- src/components/MDX/AdComponent/AdComponent.tsx
+++ src/components/MDX/AdComponent/AdComponent.tsx
@@ -11,16 +11,31 @@ const AdComponent: React.FC = () => {
     }
   }, []);
 
+  const isProduction = process.env.NODE_ENV === 'production';
+
   return (
     <>
-      <ins
-        className="adsbygoogle"
-        style={{ display: 'block', textAlign: 'center' }}
-        data-ad-layout="in-article"
-        data-ad-format="fluid"
-        data-ad-client="ca-pub-5852582960793521"
-        data-ad-slot="3845515975"
-      ></ins>
+      {isProduction ? (
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block', textAlign: 'center' }}
+          data-ad-layout="in-article"
+          data-ad-format="fluid"
+          data-ad-client="ca-pub-5852582960793521"
+          data-ad-slot="3845515975"
+        ></ins>
+      ) : (
+        <div
+          style={{
+            textAlign: 'center',
+            padding: '80px',
+            border: '1px dashed #fff',
+            color: '#fff',
+          }}
+        >
+          Ad Placeholder
+        </div>
+      )}
     </>
   );
 };
