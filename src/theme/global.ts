afa1e2ecd17ee4c3170eb31cbbb4faab8f8a4530
Oktay Shakirov <oktayshakirov@gmail.com>
2024-08-27 13:39:50 +0200

    update

--- src/theme/global.ts
+++ src/theme/global.ts
@@ -51,8 +51,8 @@ export const global = css`
     text-align: center;
     box-sizing: border-box;
     color: ${theme.palette.text.primary};
-    overflow: hidden; /* Ensures content stays inside the box */
-    width: 100%; /* Make sure ads do not exceed container width */
+    overflow: hidden;
+    width: 100%;
 
     ${theme.breakpoints.up('md')} {
       padding: ${theme.spacing(5)};
