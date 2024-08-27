09a8c5537f3c90b0b9fa709251f6eb4bd7031b37
Oktay Shakirov <oktayshakirov@gmail.com>
2024-08-27 13:36:36 +0200

    Updated ads styling

--- src/theme/global.ts
+++ src/theme/global.ts
@@ -1,6 +1,6 @@
 import { css } from '@emotion/react';
 import { colors } from './colors';
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google';
 import { theme } from '@theme/theme';
 
 const inter = Inter({ weight: ['400', '600'], subsets: ['latin'] });
@@ -11,6 +11,7 @@ export const global = css`
     color: ${theme.palette.grey[200]}!important;
     font-family: ${inter.style.fontFamily};
   }
+
   h1,
   h2,
   h3,
@@ -19,12 +20,14 @@ export const global = css`
   h6 {
     font-weight: 600;
   }
+
   input,
   textarea {
     font-family: inherit;
     font-weight: 400;
     font-size: 1rem;
   }
+
   #__next {
     min-height: 100vh;
     width: 100%;
@@ -32,7 +35,25 @@ export const global = css`
     flex-direction: column;
     position: relative;
   }
+
   main {
     flex-grow: 1;
+  }
+
+  ins.adsbygoogle {
+    display: block;
+    margin: ${theme.spacing(3)} auto;
+    max-width: 100%;
+    padding: ${theme.spacing(3)};
+    background-color: rgba(0, 0, 0, 0.05);
+    box-shadow: 0 5px 8px rgba(0, 0, 0, 0.2);
+    border-radius: 25px;
+    text-align: center;
+    box-sizing: border-box;
+    color: ${theme.palette.text.primary};
+
+    ${theme.breakpoints.up('md')} {
+      padding: ${theme.spacing(5)};
+    }
   }
 `;
