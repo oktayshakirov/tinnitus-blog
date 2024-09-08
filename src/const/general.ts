8ea3b4db1b8f2a9f91ae9a865f687cf4d8047a0d
Oktay Shakirov <oktayshakirov@gmail.com>
2024-09-09 00:33:24 +0200

    seo improvements

--- src/const/general.ts
+++ src/const/general.ts
@@ -1,4 +1,4 @@
 export const DOMAIN = 'https://www.tinnitushelp.me/';
-export const DOMAIN_NAME = 'Tinnitus Help';
+export const DOMAIN_NAME = 'TinnitusHelp.me';
 export const DEFAULT_OG_IMAGE = `/images/favicon.ico`;
 export const GA_TRACKING_ID = `G-KHGG3B9VTM`;
