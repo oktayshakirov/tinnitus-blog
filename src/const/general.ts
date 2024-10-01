ce42611907e78c51dd6189eab7f2793ef5b75229
oktayshakirov <oktayshakirov@gmail.com>
2024-10-01 03:23:55 +0200

    fixed broken links

--- src/const/general.ts
+++ src/const/general.ts
@@ -1,4 +1,4 @@
-export const DOMAIN = 'https://www.tinnitushelp.me/';
+export const DOMAIN = 'https://www.tinnitushelp.me';
 export const DOMAIN_NAME = 'Tinnitus Help';
 export const DEFAULT_OG_IMAGE = `/meta-image.png`;
 export const GA_TRACKING_ID = `G-KHGG3B9VTM`;
