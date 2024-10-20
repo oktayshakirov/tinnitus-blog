61ca9977de6fbf47183b483131ecdfb02814ea29
oktayshakirov <oktayshakirov@gmail.com>
2024-10-20 21:40:50 +0200

    undo changes

--- next.config.js
+++ next.config.js
@@ -4,19 +4,6 @@ const nextConfig = {
     emotion: true,
   },
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*).(png|jpg|js|css|woff2|ico|svg)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
