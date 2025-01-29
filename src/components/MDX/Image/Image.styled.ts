124a0584bbbf897663fd2479b70da061b691ebbe
oktayshakirov <oktayshakirov@gmail.com>
2025-01-29 20:09:59 +0100

    fixed issue with in article images

--- src/components/MDX/Image/Image.styled.ts
+++ src/components/MDX/Image/Image.styled.ts
@@ -5,15 +5,21 @@ export const StyledImageContainer = styled('div')`
   ${({ theme }) => css`
     position: relative;
     overflow: hidden;
-    height: 40vw;
+    aspect-ratio: 16 / 9;
+    width: 100%;
     border-radius: ${theme.spacing(0.5)};
 
     img {
       object-fit: cover;
+      width: 100%;
+      height: 100%;
+    }
+
+    ${theme.breakpoints.down('md')} {
+      aspect-ratio: 4 / 3;
     }
 
     ${theme.breakpoints.up('md')} {
-      height: 30vw;
       max-height: ${theme.spacing(50)};
     }
   `}
