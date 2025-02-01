8ec05b7c0e1dd44d151b526d34616131a452a7c1
Oktay Shakirov <oktayshakirov@gmail.com>
2025-02-01 12:13:23 +0100

    added isApp check

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import Script from 'next/script';
@@ -15,45 +15,58 @@ import * as gtag from '@lib/gtag';
 
 const App = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
+  const [isApp, setIsApp] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.ReactNativeWebView) {
+      setIsApp(true);
+    }
+  }, []);
 
   useEffect(() => {
     const handleRouteChange = (url: string) => {
-      gtag.pageview(url);
+      if (!isApp) {
+        gtag.pageview(url);
+      }
     };
     router.events.on('routeChangeComplete', handleRouteChange);
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange);
     };
-  }, [router.events]);
+  }, [router.events, isApp]);
 
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Script
-        strategy="afterInteractive"
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5852582960793521"
-        crossOrigin="anonymous"
-      />
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+      {!isApp && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5852582960793521"
+            crossOrigin="anonymous"
+          />
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+        </>
+      )}
 
       <DefaultSeo {...SEO_CONFIG} />
       <ThemeProvider theme={theme}>
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,14 +1,23 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
 
 type Props = {
   children: ReactNode;
 };
+
 const Layout = ({ children }: Props) => {
+  const [isApp, setIsApp] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.ReactNativeWebView) {
+      setIsApp(true);
+    }
+  }, []);
+
   return (
     <>
-      <Header />
+      {!isApp && <Header />}
       <main>{children}</main>
       <Footer />
     </>
