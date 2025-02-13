28366ea7ae5a1105676f3a9cb44c93bb9714c3a3
Oktay Shakirov <oktayshakirov@gmail.com>
2025-02-13 18:10:48 +0100

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,25 +13,8 @@ import { theme } from '@theme/theme';
 import { GA_TRACKING_ID } from '@const/general';
 import * as gtag from '@lib/gtag';
 
-function getIsAppFlag(): boolean {
-  if (typeof window === 'undefined') return false;
-  const urlParams = new URLSearchParams(window.location.search);
-  return (
-    urlParams.get('isApp') === 'true' ||
-    !!window.isApp ||
-    localStorage.getItem('isApp') === 'true'
-  );
-}
-
 const App = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
-  const isApp = getIsAppFlag();
-
-  useEffect(() => {
-    if (isApp) {
-      localStorage.setItem('isApp', 'true');
-    }
-  }, [isApp]);
 
   useEffect(() => {
     const handleRouteChange = (url: string) => {
@@ -48,35 +31,31 @@ const App = ({ Component, pageProps }: AppProps) => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <DefaultSeo {...SEO_CONFIG} />
-      {!isApp && (
-        <>
-          <Script
-            strategy="afterInteractive"
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5852582960793521"
-            crossOrigin="anonymous"
-          />
-          <Script
-            strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <Script
-            id="google-analytics"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_TRACKING_ID}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-            }}
-          />
-        </>
-      )}
+      <Script
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5852582960793521"
+        crossOrigin="anonymous"
+      />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
 
+      <DefaultSeo {...SEO_CONFIG} />
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Global styles={global} />
