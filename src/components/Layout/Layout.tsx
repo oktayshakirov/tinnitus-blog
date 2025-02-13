6a5b8a985139f28201d3235880ddedc6ddcb5a70
Oktay Shakirov <oktayshakirov@gmail.com>
2025-02-13 21:01:44 +0100

    update

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,44 +1,24 @@
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode, useState } from 'react';
 import dynamic from 'next/dynamic';
 import Footer from '@components/Footer';
 
 const Header = dynamic(() => import('@components/Header'), { ssr: true });
-
-function getIsAppFlag(): boolean {
-  if (typeof window === 'undefined') return false;
-  const urlParams = new URLSearchParams(window.location.search);
-  return (
-    urlParams.get('isApp') === 'true' ||
-    !!window.isApp ||
-    localStorage.getItem('isApp') === 'true'
-  );
-}
 
 type Props = {
   children: ReactNode;
 };
 
 const Layout = ({ children }: Props) => {
-  const [isApp, setIsApp] = useState<boolean>(() => {
+  const [isApp] = useState<boolean>(() => {
     if (typeof document !== 'undefined') {
       return document.documentElement.classList.contains('is-app');
     }
     return false;
   });
 
-  useEffect(() => {
-    if (!isApp) {
-      const flag = getIsAppFlag();
-      if (flag) {
-        setIsApp(true);
-        localStorage.setItem('isApp', 'true');
-      }
-    }
-  }, [isApp]);
-
   return (
     <>
-      <div className={isApp ? 'header-hidden' : ''}>
+      <div className="header">
         <Header />
       </div>
       <main>{children}</main>
