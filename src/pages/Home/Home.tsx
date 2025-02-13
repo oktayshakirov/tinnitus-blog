27ec47c753afa489449b9e7a0dfdd4a039581069
Oktay Shakirov <oktayshakirov@gmail.com>
2025-02-13 18:47:46 +0100

    test

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
@@ -6,19 +5,29 @@ import Logo from '@components/Logo';
 import Logo2 from '@components/Logo2';
 import Layout from '@components/Layout';
 import HomeSEO from './Home.SEO';
-import { Button, Box } from '@mui/material';
+import CardContent from '@mui/material/CardContent';
+import { Button, CardActionArea } from '@mui/material';
 import DoubleArrowRoundedIcon from '@mui/icons-material/DoubleArrowRounded';
 import SpatialTrackingIcon from '@mui/icons-material/SpatialTracking';
 import NextLink from 'next/link';
-import ArticleCard from '@components/ArticleCard';
+import Image from 'next/image';
 import {
   StyledContainer,
   StyledHeadingContainer,
   StyledLogoContainer,
+  StyledCard,
   StyledTabContainer,
   StyledTextContainer,
 } from './Home.styled';
 import { ArticleMeta } from '@types';
+import {
+  FaSpotify,
+  FaYoutube,
+  FaApple,
+  FaAmazon,
+  FaDeezer,
+} from 'react-icons/fa';
+import { Divider } from '@mui/material';
 import AdComponent from '@components/AdComponent';
 
 type HomeProps = {
@@ -27,60 +36,34 @@ type HomeProps = {
   featuredPosts: ArticleMeta[];
 };
 
-function getIsAppFlag(): boolean {
-  if (typeof window === 'undefined') return false;
-  const urlParams = new URLSearchParams(window.location.search);
-  return (
-    urlParams.get('isApp') === 'true' ||
-    !!window.isApp ||
-    localStorage.getItem('isApp') === 'true'
-  );
-}
-
 const Home = ({ latestPosts, latestZen, featuredPosts }: HomeProps) => {
-  const [isApp, setIsApp] = useState(false);
-  const [hasMounted, setHasMounted] = useState(false);
-
-  useEffect(() => {
-    const flag = getIsAppFlag();
-    if (flag) {
-      setIsApp(true);
-      localStorage.setItem('isApp', 'true');
-    }
-    setHasMounted(true);
-  }, []);
-
-  if (!hasMounted) return null;
-
   return (
     <>
       <HomeSEO />
       <Layout>
-        {!isApp && (
-          <StyledContainer>
-            <Grid container spacing={2}>
-              <StyledHeadingContainer item md={6}>
-                <Box>
-                  <Typography component="h1" variant="h3" mb={{ xs: 2, md: 2 }}>
-                    Tinnitus Help
+        <StyledContainer>
+          <Grid container spacing={2}>
+            <StyledHeadingContainer item md={6}>
+              <div>
+                <Typography component="h1" variant="h3" mb={{ xs: 2, md: 2 }}>
+                  Tinnitus Help
+                </Typography>
+                <Grid item xs={12} md={12}>
+                  <Typography component="h2" variant="h6">
+                    Discover the fascinating world of perceiving sound in the
+                    absence of an external source. Living with tinnitus can be
+                    challenging, but you&#39;re not alone. Explore valuable tips
+                    and resources to manage and cope with tinnitus.
+                    Everyone&#39;s experience with tinnitus is unique.
                   </Typography>
-                  <Grid item xs={12} md={12}>
-                    <Typography component="h2" variant="h6">
-                      Discover the fascinating world of perceiving sound in the
-                      absence of an external source. Living with tinnitus can be
-                      challenging, but you&#39;re not alone. Explore valuable
-                      tips and resources to manage and cope with tinnitus.
-                      Everyone&#39;s experience with tinnitus is unique.
-                    </Typography>
-                  </Grid>
-                </Box>
-              </StyledHeadingContainer>
-              <StyledLogoContainer item md={6}>
-                <Logo />
-              </StyledLogoContainer>
-            </Grid>
-          </StyledContainer>
-        )}
+                </Grid>
+              </div>
+            </StyledHeadingContainer>
+            <StyledLogoContainer item md={6}>
+              <Logo />
+            </StyledLogoContainer>
+          </Grid>
+        </StyledContainer>
         <Container>
           <AdComponent />
         </Container>
@@ -89,10 +72,45 @@ const Home = ({ latestPosts, latestZen, featuredPosts }: HomeProps) => {
             <Typography gutterBottom variant="h4" align="center">
               Latest Posts:
             </Typography>
-            <Grid container spacing={4}>
-              {latestPosts.map((article, index) => (
-                <Grid item xs={12} sm={6} md={4} key={article.slug}>
-                  <ArticleCard article={article} index={index} withTags />
+            <Grid container spacing={2}>
+              {latestPosts.map((article) => (
+                <Grid item xs={12} sm={4} key={article.slug}>
+                  <StyledCard>
+                    <CardActionArea>
+                      <NextLink href={`/blog/${article.slug}`} passHref>
+                        <Image
+                          src={article.image}
+                          alt={article.title}
+                          width={350}
+                          height={220}
+                          loading="lazy"
+                        />
+                      </NextLink>
+                      <CardContent>
+                        <Typography gutterBottom component="h2" variant="h6">
+                          {article.title}
+                        </Typography>
+                        <Typography variant="body1" color="text.secondary">
+                          {article.description}
+                        </Typography>
+                        <Divider sx={{ margin: '16px 0' }} />
+                        <div
+                          style={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            marginTop: '8px',
+                          }}
+                        >
+                          <Typography variant="body2" color="text.secondary">
+                            {article.readingTime.text}
+                          </Typography>
+                          <Typography variant="body2" color="text.secondary">
+                            {article.date}
+                          </Typography>
+                        </div>
+                      </CardContent>
+                    </CardActionArea>
+                  </StyledCard>
                 </Grid>
               ))}
             </Grid>
@@ -120,49 +138,116 @@ const Home = ({ latestPosts, latestZen, featuredPosts }: HomeProps) => {
             <Typography gutterBottom variant="h4" align="center">
               Must Read Posts:
             </Typography>
-            <Grid container spacing={4}>
-              {featuredPosts.map((article, index) => (
-                <Grid item xs={12} sm={6} md={4} key={article.slug}>
-                  <ArticleCard article={article} index={index} withTags />
+            <Grid container spacing={2}>
+              {featuredPosts.map((article) => (
+                <Grid item xs={12} sm={4} key={article.slug}>
+                  <StyledCard>
+                    <CardActionArea>
+                      <NextLink href={`/blog/${article.slug}`} passHref>
+                        <Image
+                          src={article.image}
+                          alt={article.title}
+                          width={350}
+                          height={220}
+                          loading="lazy"
+                        />
+                      </NextLink>
+                      <CardContent>
+                        <Typography gutterBottom component="h2" variant="h6">
+                          {article.title}
+                        </Typography>
+                        <Typography variant="body1" color="text.secondary">
+                          {article.description}
+                        </Typography>
+                        <Divider sx={{ margin: '16px 0' }} />
+                        <div
+                          style={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            marginTop: '8px',
+                          }}
+                        >
+                          <Typography variant="body2" color="text.secondary">
+                            {article.readingTime.text}
+                          </Typography>
+                          <Typography variant="body2" color="text.secondary">
+                            {article.date}
+                          </Typography>
+                        </div>
+                      </CardContent>
+                    </CardActionArea>
+                  </StyledCard>
                 </Grid>
               ))}
             </Grid>
           </StyledTabContainer>
         </Container>
-        {!isApp && (
-          <StyledContainer>
-            <Grid container spacing={2}>
-              <StyledLogoContainer item md={6}>
-                <Logo2 />
-              </StyledLogoContainer>
-              <StyledHeadingContainer item md={6}>
-                <Box>
-                  <Typography component="h2" variant="h3" mb={{ xs: 2, md: 2 }}>
-                    The Power of Sound
+        <StyledContainer>
+          <Grid container spacing={2}>
+            <StyledLogoContainer item md={6}>
+              <Logo2 />
+            </StyledLogoContainer>
+            <StyledHeadingContainer item md={6}>
+              <div>
+                <Typography component="h2" variant="h3" mb={{ xs: 2, md: 2 }}>
+                  The Power of Sound
+                </Typography>
+                <Grid item xs={12} md={12}>
+                  <Typography component="h2" variant="h6">
+                    Discover the potential benefits of Tinnitus Sound Therapy,
+                    which includes white noise, calming melodies and mindful
+                    meditation. These therapeutic sounds may help in managing
+                    tinnitus, enhancing relaxation, and promoting a sense of
+                    inner balance.
                   </Typography>
-                  <Grid item xs={12} md={12}>
-                    <Typography component="h2" variant="h6">
-                      Discover the potential benefits of Tinnitus Sound Therapy,
-                      which includes white noise, calming melodies and mindful
-                      meditation. These therapeutic sounds may help in managing
-                      tinnitus, enhancing relaxation, and promoting a sense of
-                      inner balance.
-                    </Typography>
-                  </Grid>
-                </Box>
-              </StyledHeadingContainer>
-            </Grid>
-          </StyledContainer>
-        )}
+                </Grid>
+              </div>
+            </StyledHeadingContainer>
+          </Grid>
+        </StyledContainer>
         <Container>
           <StyledTabContainer>
             <Typography gutterBottom variant="h4" align="center">
               Latest Sounds:
             </Typography>
-            <Grid container spacing={4}>
-              {latestZen.map((sounds, index) => (
-                <Grid item xs={12} sm={6} md={4} key={sounds.slug}>
-                  <ArticleCard article={sounds} index={index} />
+            <Grid container spacing={2}>
+              {latestZen.map((therapy) => (
+                <Grid item xs={12} sm={4} key={therapy.slug}>
+                  <StyledCard>
+                    <CardActionArea>
+                      <NextLink href={`/zen/${therapy.slug}`} passHref>
+                        <Image
+                          src={therapy.image}
+                          alt={therapy.title}
+                          width={350}
+                          height={220}
+                          loading="lazy"
+                        />
+                      </NextLink>
+                      <CardContent>
+                        <Typography gutterBottom component="h2" variant="h6">
+                          {therapy.title}
+                        </Typography>
+                        <Typography variant="body1" color="text.secondary">
+                          {therapy.description}
+                        </Typography>
+                        <Divider sx={{ margin: '16px 0' }} />
+                        <div
+                          style={{
+                            display: 'flex',
+                            justifyContent: 'space-around',
+                            padding: '8px 0',
+                          }}
+                        >
+                          <FaSpotify size={24} />
+                          <FaYoutube size={24} />
+                          <FaApple size={24} />
+                          <FaAmazon size={24} />
+                          <FaDeezer size={24} />
+                        </div>
+                      </CardContent>
+                    </CardActionArea>
+                  </StyledCard>
                 </Grid>
               ))}
             </Grid>
