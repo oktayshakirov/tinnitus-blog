f9b7b281b24a6c8c4365277f7f7383687f12ef64
Oktay Shakirov <oktayshakirov@gmail.com>
2025-02-20 14:36:28 +0100

    added /app page

---
+++ pages/app/index.tsx
@@ -0,0 +1,3 @@
+import App from '@ui/pages/App';
+
+export default App;
---
+++ src/pages/App/App.SEO.tsx
@@ -0,0 +1,27 @@
+import { NextSeo } from 'next-seo';
+import { DEFAULT_OG_IMAGE, DOMAIN, DOMAIN_NAME } from '@const/general';
+
+const AboutSEO = () => {
+  const title = `App | ${DOMAIN_NAME}`;
+  const description =
+    'Learn more about TinnitusHelp.me, our mission and the team behind our tinnitus resources. We are dedicated to providing support and reliable information to help manage tinnitus effectively.';
+  const canonical = `${DOMAIN}/about`;
+  const imageUrl = `${DOMAIN}${DEFAULT_OG_IMAGE}`;
+
+  return (
+    <NextSeo
+      title={title}
+      description={description}
+      canonical={canonical}
+      openGraph={{
+        url: canonical,
+        title: title,
+        description: description,
+        images: [{ url: imageUrl, type: 'image/jpeg' }],
+        siteName: DOMAIN_NAME,
+      }}
+    />
+  );
+};
+
+export default AboutSEO;
