e43a25cef82effacbd0de20f41e71a7cba090052
Oktay Shakirov <oktayshakirov@gmail.com>
2025-03-10 19:42:04 +0100

    fixed image ratio issue, new post

--- src/components/MDX/Image/Image.styled.ts
+++ src/components/MDX/Image/Image.styled.ts
@@ -5,9 +5,21 @@ export const StyledImageContainer = styled('div')`
   ${({ theme }) => css`
     position: relative;
     overflow: hidden;
-    aspect-ratio: 16 / 9;
     width: 100%;
     border-radius: ${theme.spacing(0.5)};
     min-height: 200px;
+    aspect-ratio: 16 / 9;
+
+    @media (max-width: ${theme.breakpoints.values.sm}px) {
+      aspect-ratio: 4 / 3;
+      min-height: 180px;
+    }
+
+    img {
+      object-fit: cover;
+      object-position: center;
+      width: 100% !important;
+      height: 100% !important;
+    }
   `}
 `;
