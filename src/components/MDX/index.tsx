7ab83d42158254978a58c4110917282e7ce7b558
Oktay Shakirov <oktayshakirov@gmail.com>
2025-05-30 19:04:53 +0200

    new post, mdx font

--- src/components/MDX/index.tsx
+++ src/components/MDX/index.tsx
@@ -1,5 +1,7 @@
 import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote';
 import { MDXComponents } from 'mdx/types';
+import { styled } from '@mui/material/styles';
+import { css } from '@emotion/react';
 import Link from '@components/MDX/Link';
 import { H1, H2, H3, H4, H5, H6 } from '@components/MDX/Heading';
 import Nav from '@components/MDX/Nav';
@@ -9,6 +11,39 @@ import Highlighter from '@components/MDX/Highlighter';
 import AdComponent from '@components/MDX/AdComponent';
 import StreamingPlatforms from './StreamingPlatforms';
 import Table from '@components/MDX/Table';
+
+const StyledMDXWrapper = styled('div')`
+  ${({ theme }) => css`
+    font-family: 'Nunito', sans-serif;
+    font-size: 1rem;
+    line-height: 1.7;
+    color: ${theme.palette.text.primary};
+
+    p {
+      margin: ${theme.spacing(2)} 0;
+    }
+
+    ul,
+    ol {
+      margin: ${theme.spacing(2)} 0;
+      padding-left: ${theme.spacing(3)};
+    }
+
+    li {
+      margin: ${theme.spacing(1)} 0;
+    }
+
+    a {
+      color: ${theme.palette.primary.main};
+      text-decoration: none;
+      transition: color 0.2s ease;
+
+      &:hover {
+        color: ${theme.palette.primary.light};
+      }
+    }
+  `}
+`;
 
 export const components: MDXComponents = {
   a: Link,
@@ -30,8 +65,13 @@ export const components: MDXComponents = {
 type Props = {
   content: MDXRemoteProps;
 };
+
 const MDXContent = ({ content }: Props) => {
-  return <MDXRemote components={components} {...content} />;
+  return (
+    <StyledMDXWrapper>
+      <MDXRemote components={components} {...content} />
+    </StyledMDXWrapper>
+  );
 };
 
 export default MDXContent;
