1d671216d18c5055ed1dd558d58fdd990e8c0f74
Oktay Shakirov <oktayshakirov@gmail.com>
2025-05-30 22:45:09 +0200

    added pagination to TagArticles

--- src/pages/TagArticles/TagArticles.tsx
+++ src/pages/TagArticles/TagArticles.tsx
@@ -1,33 +1,76 @@
+import React from 'react';
 import Container from '@mui/material/Container';
 import Layout from '@components/Layout';
-import { Article } from '@types';
+import { ArticleMeta } from '@types';
 import Typography from '@mui/material/Typography';
 import ArticlesGrid from '@components/ArticlesGrid';
 import Box from '@mui/material/Box';
 import AdComponent from '@components/AdComponent';
 import TagArticlesSEO from './TagArticles.SEO';
-import { StyledContainer, StyledTabContainer } from './TagArticles.styled';
+import {
+  StyledContainer,
+  StyledTabContainer,
+  StyledPagination,
+} from './TagArticles.styled';
+import PaginationItem from '@mui/material/PaginationItem';
+import Link from '@components/Link';
+import { kebabize } from '@lib/strings';
+
+export type TagArticlesProps = {
+  tagSlug: string;
+  articles: ArticleMeta[];
+  page: number;
+  pageCount: number;
+};
 
 const TagArticles = ({
-  slug,
+  tagSlug,
   articles,
-}: {
-  slug: string;
-  articles: Article[];
-}) => {
-  const articlesToDisplay = articles.map((article) => article.meta);
+  page,
+  pageCount,
+}: TagArticlesProps) => {
+  const kebabizedTagForLink = kebabize(tagSlug);
 
   return (
     <>
-      <TagArticlesSEO tag={slug} />
+      <TagArticlesSEO tag={tagSlug} />
       <Layout>
         <StyledContainer>
           <StyledTabContainer>
             <Container>
-              <Typography component="h1" variant="h4" gutterBottom>
-                Articles tagged with: {slug}
+              <Typography
+                component="h1"
+                variant="h4"
+                gutterBottom
+                sx={{ textTransform: 'capitalize' }}
+              >
+                Articles tagged with: {tagSlug}
               </Typography>
-              <ArticlesGrid articles={articlesToDisplay} />
+              {articles.length > 0 ? (
+                <ArticlesGrid articles={articles} />
+              ) : (
+                <Typography sx={{ mt: 2 }}>
+                  No articles found for this tag on this page.
+                </Typography>
+              )}
+              {pageCount > 1 && (
+                <StyledPagination
+                  count={pageCount}
+                  page={page}
+                  color="primary"
+                  hidePrevButton
+                  hideNextButton
+                  renderItem={(item) => {
+                    let href = `/tags/${kebabizedTagForLink}`;
+                    if (item.page && item.page > 1) {
+                      href += `--page-${item.page}`;
+                    }
+                    return (
+                      <PaginationItem component={Link} href={href} {...item} />
+                    );
+                  }}
+                />
+              )}
             </Container>
           </StyledTabContainer>
           <Container>
