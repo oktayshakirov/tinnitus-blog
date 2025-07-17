d66a7d8c2b3a978be8b4794451020fa2a63d10f5
Oktay Shakirov <oktayshakirov@gmail.com>
2025-07-17 11:26:19 +0200

    Add caching headers for static assets in next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,28 @@ const nextConfig = {
     emotion: true,
   },
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/_next/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/images/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
