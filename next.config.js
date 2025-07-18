898597438d642abb5ca11dae53c4a23e09132100
Oktay Shakirov <oktayshakirov@gmail.com>
2025-07-18 12:40:47 +0200

    updated next.config

--- next.config.js
+++ next.config.js
@@ -26,6 +26,12 @@ const nextConfig = {
       },
     ];
   },
+  images: {
+    deviceSizes: [332, 400, 450, 640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96],
+    formats: ['image/webp', 'image/avif'],
+    domains: ['www.tinnitushelp.me'],
+  },
 };
 
 module.exports = nextConfig;
